36a00e2c615ae85f7af3215eae5ec18364bb2173
Muga20 <murimibrian18@outlook.com>
2023-06-04 13:16:07 +0300

    Tutorial Start

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, SafeAreaView ,Image  } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+      <Text style={styles.hello}>
+        Changes you make will automatically reload.
+      </Text>
+
+      <Button title="Click me" onPress={() => alert("Hello World")} />
       <StatusBar style="auto" />
-    </View>
+
+   
+
+      <Image source={require("./assets/icon.png")} style={{ width: 40, height: 40 }} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  hello: {
+    color: "red",
+    fontSize: 20,
+    backgroundColor: "yellow",
+    height: 50,
+  },
+  button: {
+    color: "red",
+    fontSize: 20,
+    backgroundColor: "yellow",
+    height: 50,
   },
 });
